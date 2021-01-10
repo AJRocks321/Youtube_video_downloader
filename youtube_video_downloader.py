88912741403cbb02619d4320c48e77d50dffc622
AJRocks123M <70801883+AJRocks123M@users.noreply.github.com>
2021-01-10 16:24:45 +0530

    version 1.01:feature: user defined location to save video

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -10,10 +10,10 @@ i=1
 for stream in videos:
     print(str(i) +" "+str(stream))
     i += 1
-
 stream_number = int(input("enter number"))
 video = videos[stream_number-1]
 
-
-video.download("D:/mit differential lectures")
+#give saving location here
+path=input("give location you want to save:")
+video.download("input")
 print("Downloaded")
